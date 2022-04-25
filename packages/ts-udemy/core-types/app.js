ee6b308816483fc12f48511a55b74fdecf85315d
Samuel Chen <samuel.chen@akunacapital.com>
2022-04-25 23:54:42 +1000

    feat: end of lecture 18

--- packages/ts-udemy/core-types/app.js
+++ packages/ts-udemy/core-types/app.js
@@ -1,5 +1,13 @@
+// const person: { name: string; age: number } = {
 var person = {
   name: "Samuel",
   age: 20,
+  hobbies: ["Sports", "Cooking"],
 };
+var favouriteActivities;
+favouriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+  var hobby = _a[_i];
+  console.log(hobby.toUpperCase());
+}
--- packages/ts-udemy/core-types/app.ts
+++ packages/ts-udemy/core-types/app.ts
@@ -1,6 +1,15 @@
-const person: { name: string; age: number } = {
+// const person: { name: string; age: number } = {
+const person = {
   name: "Samuel",
   age: 20,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
