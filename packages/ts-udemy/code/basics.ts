9ae9b476668bfec8e745effa30565f40f442bca4
Samuel Chen <samuel.chen@akunacapital.com>
2022-04-26 00:10:10 +1000

    chore: rename core-types folder

--- packages/ts-udemy/code/basics.ts
+++ packages/ts-udemy/code/basics.ts
@@ -10,6 +10,6 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: ';
+const resultPhrase = "Result is: ";
 
 add(number1, number2, printResult, resultPhrase);
