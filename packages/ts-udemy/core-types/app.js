5ac12966fe833c2b69309c0df3412e925a68af85
Samuel Chen <samuel.chen@akunacapital.com>
2022-04-25 23:35:47 +1000

    feat: start of lect 16

---
+++ packages/ts-udemy/core-types/app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: 'Samuel',
+    age: 20
+};
+console.log(person.nickname);
---
+++ packages/ts-udemy/core-types/app.ts
@@ -0,0 +1,6 @@
+const person = {
+  name: 'Samuel', 
+  age: 20,
+};
+
+console.log(person.nickname);
