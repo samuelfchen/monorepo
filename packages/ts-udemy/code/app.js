722cb577b50bcc0b01d00dd030f88d5160f4cd31
Samuel Chen <samuel.chen@akunacapital.com>
2022-04-26 00:31:36 +1000

    feat: end lec 24 (type aliases)

--- packages/ts-udemy/code/app.js
+++ packages/ts-udemy/code/app.js
@@ -9,11 +9,6 @@ function combine(input1, input2, resultConversion) {
     result = input1.toString() + input2.toString();
   }
   return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result
-  // } else {
-  //   return result.toString()
-  // }
 }
 var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
--- packages/ts-udemy/code/app.ts
+++ packages/ts-udemy/code/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-string";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-string"
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
   if (
