987190a8e2f1cd98dd7505d706c2810cb25514b2
Samuel Chen <samuel.chen@akunacapital.com>
2022-04-26 00:07:13 +1000

    feat: end lect 20 (enums)

--- packages/ts-udemy/core-types/app.js
+++ packages/ts-udemy/core-types/app.js
@@ -1,13 +1,31 @@
-// const person: { name: string; age: number } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   // const person = {
+//   name: "Samuel",
+//   age: 20,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+  Role[(Role["ADMIN"] = 0)] = "ADMIN";
+  Role[(Role["READ_ONLY"] = 1)] = "READ_ONLY";
+  Role[(Role["AUTHOR"] = 2)] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
   name: "Samuel",
   age: 20,
   hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
 var favouriteActivities;
 favouriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
   var hobby = _a[_i];
-  console.log(hobby.toUpperCase());
+  console.log(hobby);
 }
--- packages/ts-udemy/core-types/app.ts
+++ packages/ts-udemy/core-types/app.ts
@@ -1,14 +1,27 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  // const person = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   // const person = {
+//   name: "Samuel",
+//   age: 20,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Samuel",
   age: 20,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 let favouriteActivities: string[];
