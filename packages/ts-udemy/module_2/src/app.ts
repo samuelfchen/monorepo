07c041a19a1064c08efa6366759b3208ee1d74a2
Samuel Chen <samuel.chen@akunacapital.com>
2022-04-27 17:35:38 +1000

    feat: end lec 44 (all tsconfig stuff)

--- packages/ts-udemy/module_2/src/app.ts
+++ packages/ts-udemy/module_2/src/app.ts
@@ -1,5 +1,7 @@
-const button = document.querySelector("button");
+const button = document.querySelector("button")!; // ! means you know it will yield a non null value
 
-button?.addEventListener("click", () => {
-  console.log("Clicked!");
-});
+if (button) {
+  button?.addEventListener("click", () => {
+    console.log("Clicked!");
+  });
+}
