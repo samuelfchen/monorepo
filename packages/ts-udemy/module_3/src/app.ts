936601cdf2bed228338cddc1999cdac1bbcda6f7
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-02 12:41:33 +1000

    feat: end lec 67 (getters and setters)

--- packages/ts-udemy/module_3/src/app.ts
+++ packages/ts-udemy/module_3/src/app.ts
@@ -26,8 +26,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addEmployee(name: string) {
@@ -39,6 +56,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -57,7 +75,10 @@ it.printEmployeeInformation();
 console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
+// console.log(accounting.mostRecentReport)
 accounting.addReport("Something went wrong!");
+accounting.mostRecentReport = "Year end report";
+console.log(accounting.mostRecentReport);
 accounting.printReports();
 
 accounting.addEmployee("Max");
