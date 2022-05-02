01f94aedd0927a722f17e3357cf92eb754252898
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-02 12:22:02 +1000

    feat: end lec 62 (private)

--- packages/ts-udemy/module_3/src/app.ts
+++ packages/ts-udemy/module_3/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,21 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
-
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-
-accountingCopy.describe();
-
 accounting.describe();
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.printEmployeeInformation();
