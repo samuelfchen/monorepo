c7c3ab40bfefdf27a5f1e173735c66780141fea3
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-02 12:17:26 +1000

    feat: end lec 61 (basic class and this)

---
+++ packages/ts-udemy/module_3/src/app.ts
@@ -0,0 +1,19 @@
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+accountingCopy.describe();
+
+accounting.describe();
