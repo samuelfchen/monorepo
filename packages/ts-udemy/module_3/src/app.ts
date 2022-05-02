99698935e48bdccee329c76239f713d7fdf1fdc2
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-02 20:28:49 +1000

    feat: end lec 78 (optional params)

--- packages/ts-udemy/module_3/src/app.ts
+++ packages/ts-udemy/module_3/src/app.ts
@@ -11,7 +11,8 @@ add = (n1: number, n2: number) => {
 };
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -19,20 +20,26 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 20;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("No name provided");
+    }
   }
 }
 
 let user1: Greetable;
-user1 = new Person("Sam");
+user1 = new Person();
 user1.greet("Hi there - I am: number");
 
 console.log(user1);
