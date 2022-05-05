83e618de1dfdd09ef20263a39747b9fc5cfb2812
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-05 22:06:13 +1000

    feat: end lec 83 (intersection types)

---
+++ packages/ts-udemy/module_4_advanced_types/src/app.ts
@@ -0,0 +1,26 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+const e1: ElevatedEmployee = {
+  name: "Sam",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+console.log("this worked!");
