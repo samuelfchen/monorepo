f8cbde1b2074065f3dfc44523daf4917a3aef671
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-05 22:28:49 +1000

    feat: end lec 86 (type casting)

--- packages/ts-udemy/module_4_advanced_types/src/app.ts
+++ packages/ts-udemy/module_4_advanced_types/src/app.ts
@@ -103,3 +103,13 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+const paragraph = document.getElementById("message-output");
+
+// const userInputElement = <HTMLInputElement> document.getElementById("user-input")!;
+// const userInputElement = document.getElementById("user-input") as HTMLInputElement; // react alternative
+const userInputElement = document.getElementById("user-input");
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
