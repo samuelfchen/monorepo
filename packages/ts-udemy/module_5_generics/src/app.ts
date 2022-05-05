1a53db0e562d860c09692142fd93c33d86fec485
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-05 23:33:40 +1000

    feat: end lec 97 (another generic function)

--- packages/ts-udemy/module_5_generics/src/app.ts
+++ packages/ts-udemy/module_5_generics/src/app.ts
@@ -16,3 +16,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "Sam" }, { age: 21 });
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe([]));
