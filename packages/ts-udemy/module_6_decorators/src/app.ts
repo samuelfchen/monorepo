4bb8c1dc5888560b601200604b716cf0f8edb176
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-06 14:08:36 +1000

    feat: end lec 108 (end intro to decorators)

--- packages/ts-udemy/module_6_decorators/src/app.ts
+++ packages/ts-udemy/module_6_decorators/src/app.ts
@@ -1,9 +1,27 @@
-function Logger(constructor: Function) {
-  console.log("Logging...");
-  console.log(constructor);
+function Logger(logString: string) {
+  console.log("Logger factory");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function withTemplate(template: string, hookId: string) {
+  console.log("Template factory");
+  return function (constructor: any) {
+    console.log("Rendering template...");
+
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+@Logger("LOGGING - PERSON")
+@withTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Sam";
 
