03f3bdc6d764ab59fa2e3badef8b94ec9d791370
Samuel Chen <samuel.chen@akunacapital.com>
2022-05-06 14:33:10 +1000

    feat: end lec 112 (changing a class in a class decorator)

--- packages/ts-udemy/module_6_decorators/src/app.ts
+++ packages/ts-udemy/module_6_decorators/src/app.ts
@@ -8,15 +8,22 @@ function Logger(logString: string) {
 
 function withTemplate(template: string, hookId: string) {
   console.log("Template factory");
-  return function (constructor: any) {
-    console.log("Rendering template...");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
 
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+        console.log("Rendering template...");
+
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -94,3 +101,8 @@ class Product {
     return this._price + 1 * tax;
   }
 }
+
+const p1 = new Product("Book 1", 1);
+const p2 = new Product("Book 2", 2);
+
+console.log(p1, p2);
